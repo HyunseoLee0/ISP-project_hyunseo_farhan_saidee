024de76d2bf7244bed89bcce93cf4387ac65f968
Farhan Ishraque <farhan.ishraque@student.allenisd.org>
2022-05-09 10:16:09 -0500

    Move the block

--- Sources/ScenesShell/Block.swift
+++ Sources/ScenesShell/Block.swift
@@ -9,10 +9,12 @@ class Block: RenderableEntity,KeyDownHandler
     var x: Int
     var y: Int
     let aquaColor = Color(red:0,green:255,blue:255)
+    var drawMainScreen = true
+    var count = 20
     init(blockKind:String)
     {
         self.blockKind = blockKind
-        self.x = canvasWidth / 2 + 30
+        self.x = canvasWidth / 2 + 21
         self.y = 100
         // Using a meaningful name can be helpful for debugging
         super.init(name:"Button")
@@ -25,6 +27,18 @@ class Block: RenderableEntity,KeyDownHandler
     {
         canvasWidth = canvasSize.width
         canvasHeight = canvasSize.height
+        if !drawMainScreen
+        {
+            if count > 0
+            {
+                count -= 1
+                if count == 0 && y + 60 <= 700
+                {
+                    y += 30
+                    count = 20
+                }
+            }
+        }
     }
     override func setup(canvasSize:Size,canvas:Canvas)
     {
@@ -36,35 +50,38 @@ class Block: RenderableEntity,KeyDownHandler
     }
     func drawBlock(canvas:Canvas)
     {
-        if blockKind == "L"
-        {
-            let side = 30
-            for _ in 0 ..< 10
+        if !drawMainScreen
+        {            
+            if blockKind == "L"
             {
-                canvas.render(FillStyle(color:aquaColor),StrokeStyle(color:Color(.black)),LineWidth(width:3))
-                canvas.render(Rectangle(rect:Rect(topLeft:Point(x:x,y:y),size:Size(width:side,height:side)),fillMode:.fillAndStroke))
-                x += side
+                let side = 30
+                for _ in 0 ..< 4
+                {
+                    canvas.render(FillStyle(color:aquaColor),StrokeStyle(color:Color(.black)),LineWidth(width:3))
+                    canvas.render(Rectangle(rect:Rect(topLeft:Point(x:x,y:y),size:Size(width:side,height:side)),fillMode:.fillAndStroke))
+                    x += side
+                }
+                x -= side * 4
             }
-            x -= side * 11
         }
     }
     func onKeyDown(key:String,code:String,ctrlKey:Bool,shiftKey:Bool,altKey:Bool,metaKey:Bool)
     {
-        if key == "ArrowUp"
+        if drawMainScreen
         {
-            y -= 30
+            drawMainScreen = false
         }
         if key == "ArrowDown"
         {
             y += 30
         }
-        if key == "ArrowLeft"
+        if key == "ArrowLeft" && x - 30 > canvasWidth / 2
         {
-            x -= 1
+            x -= 30
         }
-        if key == "ArrowRight"
+        if key == "ArrowRight" && x + 30 < canvasWidth / 2 + 240
         {
-            x += 1
+            x += 30
         }
     }
 }
